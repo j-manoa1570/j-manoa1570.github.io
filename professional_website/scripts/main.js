551c4d4b746039a43301f2183a9932d5c11dacf2
Jonathan <jester1570@gmail.com>
2019-10-01 12:23:06 -0600

    Added headers and footers. Footers have copyright and last updated added

--- professional_website/scripts/main.js
+++ professional_website/scripts/main.js
@@ -16,3 +16,11 @@ if (hour >= 18 && hour < 20) {
 }
 
 //alert(welcome);
+
+var lastupdate = document.lastModified;
+var lu = document.querySelector(".lastupdate");
+lu.innerHTML = lastupdate;
+
+var currentyear = new Date().getFullYear();
+var cy = document.querySelector(".currentyear");
+cy.innerHTML = currentyear;
